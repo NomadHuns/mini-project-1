ee3c36b91b950f51a26ddba34d4fc0e4542c8c83
K-Minho <alsgh9979@naver.com>
2023-02-24 09:47:55 +0900

    join과 관련된 기능 생성

--- src/main/java/shop/mtcoding/jobara/service/UserService.java
+++ src/main/java/shop/mtcoding/jobara/service/UserService.java
@@ -2,9 +2,10 @@ package shop.mtcoding.jobara.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import shop.mtcoding.jobara.dto.user.UserReq.UserJoinReqDto;
 import shop.mtcoding.jobara.dto.user.UserReq.UserLoginReqDto;
-import shop.mtcoding.jobara.ex.CustomException;
 import shop.mtcoding.jobara.model.User;
 import shop.mtcoding.jobara.model.UserRepository;
 import shop.mtcoding.jobara.util.Verify;
@@ -15,10 +16,20 @@ public class UserService {
       @Autowired
       private UserRepository userRepository;
 
+      @Transactional(readOnly = true)
       public User login(UserLoginReqDto userLoginReqDto) {
             User userPS = userRepository.findByUsernameAndPassword(
                         new User(userLoginReqDto.getUsername(), userLoginReqDto.getPassword()));
             Verify.validateObject(userPS, "유저네임이나 암호를 확인해주세요.");
             return userPS;
+      }
+
+      @Transactional
+      public int join(UserJoinReqDto userJoinReqDto) {
+            int result = userRepository.insert(new User(userJoinReqDto.getUsername(),
+                        userJoinReqDto.getPassword(),
+                        userJoinReqDto.getEmail()));
+            Verify.validateObject(result, "유저네임이나 암호를 확인해주세요.");
+            return result;
       }
 }
