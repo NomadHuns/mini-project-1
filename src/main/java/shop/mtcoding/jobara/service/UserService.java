8aaa9a16a909f629d473f9427c9bf2dd439c215d
K-Minho <alsgh9979@naver.com>
2023-02-24 10:02:09 +0900

    Service 의 오류처리 수정

--- src/main/java/shop/mtcoding/jobara/service/UserService.java
+++ src/main/java/shop/mtcoding/jobara/service/UserService.java
@@ -1,11 +1,13 @@
 package shop.mtcoding.jobara.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import shop.mtcoding.jobara.dto.user.UserReq.UserJoinReqDto;
 import shop.mtcoding.jobara.dto.user.UserReq.UserLoginReqDto;
+import shop.mtcoding.jobara.ex.CustomException;
 import shop.mtcoding.jobara.model.User;
 import shop.mtcoding.jobara.model.UserRepository;
 import shop.mtcoding.jobara.util.Verify;
@@ -29,7 +31,9 @@ public class UserService {
             int result = userRepository.insert(new User(userJoinReqDto.getUsername(),
                         userJoinReqDto.getPassword(),
                         userJoinReqDto.getEmail()));
-            Verify.validateObject(result, "서버에 일시적인 문제가 생겼습니다");
+            if (result != 1) {
+                  throw new CustomException("서버에 일시적인 문제가 생겼습니다", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
             return result;
       }
 }
