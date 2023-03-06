f889e93a6a7ff2a850a415f4b0ae33716c5f89b7
kimalss22 <kimalss22@gmail.com>
2023-03-06 18:40:59 +0900

    테스트 코드 수정중

--- src/test/java/shop/mtcoding/jobara/employee/EmployeeControllerTest.java
+++ src/test/java/shop/mtcoding/jobara/employee/EmployeeControllerTest.java
@@ -5,15 +5,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import shop.mtcoding.jobara.employee.dto.EmployeeReq.EmployeeUpdateReqDto;
 import shop.mtcoding.jobara.user.vo.UserVo;
 
 @AutoConfigureMockMvc
@@ -23,17 +26,20 @@ public class EmployeeControllerTest {
       @Autowired
       private MockMvc mvc;
 
+      @Mock
+      private EmployeeUpdateReqDto employeeUpdateReqDto;
+
       private MockHttpSession mockSession;
 
       @BeforeEach
       public void setUp() {
-            UserVo pricipal = new UserVo();
-            pricipal.setId(1);
-            pricipal.setUsername("ssar");
-            pricipal.setRole("employee");
-            pricipal.setProfile(null);
+            UserVo principal = new UserVo();
+            principal.setId(1);
+            principal.setUsername("ssar");
+            principal.setRole("employee");
+            principal.setProfile(null);
             mockSession = new MockHttpSession();
-            mockSession.setAttribute("principal", pricipal);
+            mockSession.setAttribute("principal", principal);
       }
 
       @Test
@@ -51,32 +57,25 @@ public class EmployeeControllerTest {
       @Test
       public void update_test() throws Exception {
             // given
+            int id = 1;
             String requestBody = "password=1234&email=ssar@nate.com&realName=김살&address=부산시&detailAddress=12구&tel=01099876554&career=2&education=고졸";
             String requestBody2 = "password=1234&email=&tel=01099876554&career=2"; // null 테스트
-
+            MockMultipartFile file = new MockMultipartFile(
+                        "file", // 파라미터 이름
+                        "filename.txt", // 파일 이름
+                        "text/plain", // 파일 타입
+                        "Test data".getBytes() // 파일 내용
+            );
             // when
-            ResultActions resultActions = mvc.perform(post("/employee/update").content(requestBody)
+            ResultActions resultActions = mvc.perform(post("/employee/update/"+id).content(requestBody)
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE).session(mockSession));
-            ResultActions resultActions2 = mvc.perform(post("/employee/update").content(requestBody2)
+            ResultActions resultActions2 = mvc.perform(post("/employee/update/"
+                        + id).content(requestBody2)
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE).session(mockSession));
 
             // then
             resultActions.andExpect(status().is3xxRedirection());
             resultActions2.andExpect(status().is4xxClientError());
-
-      }
-
-      @Test
-      public void login_test() throws Exception {
-            // given
-            String requestBody = "username=ssar&password=1234";
-
-            // when
-            ResultActions resultActions = mvc.perform(post("/employee/login").content(requestBody)
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE));
-
-            // then
-            resultActions.andExpect(status().is3xxRedirection());
 
       }
 }
