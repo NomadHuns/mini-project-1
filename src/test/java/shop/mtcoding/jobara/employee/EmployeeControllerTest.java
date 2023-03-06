ac100d89b73eb327db6354da8a73039ca3c0000f
kimalss22 <kimalss22@gmail.com>
2023-03-06 23:31:59 +0900

    세션 확인 AOP 생성

--- src/test/java/shop/mtcoding/jobara/employee/EmployeeControllerTest.java
+++ src/test/java/shop/mtcoding/jobara/employee/EmployeeControllerTest.java
@@ -1,5 +1,6 @@
 package shop.mtcoding.jobara.employee;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -58,24 +59,26 @@ public class EmployeeControllerTest {
       public void update_test() throws Exception {
             // given
             int id = 1;
-            String requestBody = "password=1234&email=ssar@nate.com&realName=김살&address=부산시&detailAddress=12구&tel=01099876554&career=2&education=고졸";
-            String requestBody2 = "password=1234&email=&tel=01099876554&career=2"; // null 테스트
             MockMultipartFile file = new MockMultipartFile(
-                        "file", // 파라미터 이름
-                        "filename.txt", // 파일 이름
-                        "text/plain", // 파일 타입
-                        "Test data".getBytes() // 파일 내용
+                        "profile", // 파라미터 이름은 프론트엔드에서 정해진 대로 "profile"로 설정합니다.
+                        "filename.txt", // 파일 이름은 테스트를 위해 아무 값이나 설정합니다.
+                        "image/jpeg", // 파일 타입은 이미지 파일인 jpeg로 설정합니다.
+                        "Test data".getBytes() // 파일 내용은 테스트를 위해 아무 값이나 설정합니다.
             );
+
             // when
-            ResultActions resultActions = mvc.perform(post("/employee/update/"+id).content(requestBody)
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE).session(mockSession));
-            ResultActions resultActions2 = mvc.perform(post("/employee/update/"
-                        + id).content(requestBody2)
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE).session(mockSession));
+            ResultActions resultActions = mvc.perform(multipart("/employee/update/" + id)
+                        .file(file) // 파일을 첨부합니다.
+                        .param("password", "1234")
+                        .param("email", "ssar@nate.com")
+                        .param("address", "부산시")
+                        .param("detailAddress", "12구")
+                        .param("tel", "01099876554")
+                        .param("career", "2")
+                        .param("education", "고졸")
+                        .session(mockSession));
 
             // then
             resultActions.andExpect(status().is3xxRedirection());
-            resultActions2.andExpect(status().is4xxClientError());
-
       }
 }
